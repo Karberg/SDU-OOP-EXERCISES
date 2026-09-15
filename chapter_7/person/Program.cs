d29ecf54fbb41e630d18d6493c60c7b7971809d8
Jeppe Holm Karberg <jeppehkarberg@gmail.com>
2026-09-15 20:09:40 +0200

    Finished exercise 7.15

--- chapter_7/person/Program.cs
+++ chapter_7/person/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Top level statements
+Person a = new Person {age=10, isMale=true, name="Ryan"};
+Console.WriteLine(a.name + " is " + a.age + " years old");
+
+// Top level statements have to be before type declarations
+
+// Type declaration
+class Person {
+    public int age;
+    public bool isMale;
+    public string? name;
+}
