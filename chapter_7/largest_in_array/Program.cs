ca85d6dd1a63872003a189423fc8ebf749b02b04
Jeppe Holm Karberg <jeppehkarberg@gmail.com>
2026-09-15 19:49:40 +0200

    Finished exercise 7.4

--- chapter_7/largest_in_array/Program.cs
+++ chapter_7/largest_in_array/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Find the largest value in an array of ints
+
+int[] arr = [1, 3, 2, 5, 0, 2];
+int largest = 0;
+int largestNum = 0;
+
+// Run a for loop as long as the array length
+for (int i = 0; i < arr.GetLength(0); i++) {
+    // Check if the largest variable is smaller than the current value.
+    if (largest < arr[i]) {
+        largest = i;
+        largestNum = arr[i];
+    }
+}
+Console.WriteLine("Length: " + arr.GetLength(0));
+
+Console.WriteLine("Index of largest: " + largest);
+Console.WriteLine("Largest number in array: " + largestNum);
