5c89a4cbe729e13052e3182ce857fc7219871e6a
Jeppe Holm Karberg <jeppehkarberg@gmail.com>
2026-09-23 09:10:36 +0200

    Finished initial exercises in chapter 8

---
+++ chapter_8/sudoku_prettyprinter/Program.cs
@@ -0,0 +1,26 @@
+﻿int[][] puzzle = {
+new int[] {7, 3, 6, 4, 5, 2, 9, 8, 1},
+new int[] {1, 9, 8, 6, 3, 7, 4, 5, 2},
+new int[] {4, 2, 5, 9, 8, 1, 3, 7, 6},
+new int[] {3, 6, 4, 5, 2, 8, 1, 9, 7},
+new int[] {9, 5, 2, 7, 1, 4, 6, 3, 8},
+new int[] {8, 1, 7, 3, 9, 6, 2, 4, 5},
+new int[] {2, 8, 9, 1, 7, 3, 5, 6, 4},
+new int[] {6, 7, 3, 2, 4, 5, 8, 1, 9},
+new int[] {5, 4, 1, 8, 6, 9, 7, 2, 3},
+};
+
+
+void printPuzzle(int[][] pzl)
+{
+    for (int i = 0; i < pzl.Length; i++)
+    {
+        for (int j = 0; j < pzl[i].Length; j++)
+        {
+            Console.Write(pzl[i][j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+printPuzzle(puzzle);
---
+++ chapter_8/sum/Program.cs
@@ -0,0 +1,9 @@
+﻿int sum(int a, int b)
+{
+    return a + b;
+}
+
+int x = 12;
+int y = 10;
+
+Console.WriteLine(sum(x, y));
