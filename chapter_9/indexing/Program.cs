40c25967c0ae5b510252149a45f0202f79f2768c
Jeppe Holm Karberg <jeppehkarberg@gmail.com>
2026-09-24 13:32:28 +0200

    Chapter 9 prep is done

---
+++ chapter_9/indexing/Program.cs
@@ -0,0 +1,16 @@
+﻿int iterationer = 10;
+int[] array = {1, 2, 3, 4, 5};
+
+// increment
+for (int i=0 ; i<iterationer ; i++) {
+    try {
+        array[i]++;
+	} catch (IndexOutOfRangeException) {
+	    Console.WriteLine("The index is outside the array.");
+	}
+}
+
+// print
+for (int i=0 ; i<array.Length ; i++) {
+	Console.WriteLine(array[i]);
+}
